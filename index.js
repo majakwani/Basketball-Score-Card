53fbc3f2deb3e5a1f898089d4235891619fa2e90
majakwani <anasjakwani72@gmail.com>
2022-11-24 04:33:51 +0500

    Wrote scripts for each button to make it user interactive

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-console.log("Connected")
+let homeScore = document.querySelector(".home-score")
+let guestScore = document.querySelector(".guest-score")
+let homeScoreCount = 0
+let guestScoreCount = 0
+
+function homeOne(){
+    homeScoreCount += 1
+    homeScore.textContent = homeScoreCount
+}
+
+function homeTwo(){
+    homeScoreCount += 2
+    homeScore.textContent = homeScoreCount
+}
+
+function homeThree(){
+    homeScoreCount += 3
+    homeScore.textContent = homeScoreCount
+}
+
+function guestOne(){
+    guestScoreCount += 1
+    guestScore.textContent = guestScoreCount
+}
+
+function guestTwo(){
+    guestScoreCount += 2
+    guestScore.textContent = guestScoreCount
+}
+
+function guestThree(){
+    guestScoreCount += 3
+    guestScore.textContent = guestScoreCount
+}
+
+function reset(){
+    homeScoreCount = 0
+    guestScoreCount = 0
+    homeScore.textContent = homeScoreCount
+    guestScore.textContent = guestScoreCount
+}
